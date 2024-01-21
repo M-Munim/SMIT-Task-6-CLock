69840084c393b20c89d2debeb5ae6d98bf9b4abe
munim <muhammadmunimoff330@gmail.com>
2024-01-21 17:46:51 +0500

    smit clock

--- static/script.js
+++ static/script.js
@@ -17,21 +17,21 @@
 // }, 1000);
 
 setInterval(() => {
-const a = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const date = a.toLocaleDateString(undefined, options);
+    const a = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const date = a.toLocaleDateString(undefined, options);
 
-let hours = a.getHours();
-console.log(hours)
-const ampm = hours >= 12 ? 'PM' : 'AM';
+    let hours = a.getHours();
+    // console.log(hours)
+    const ampm = hours >= 12 ? 'PM' : 'AM';
 
-// Convert to 12-hour format
-hours = hours % 12;
-console.log(hours)
-hours = hours ? hours : 12; // Set 12 for midnight
+    // Convert to 12-hour format
+    hours = hours % 12;
+    // console.log(hours)
+    hours = hours ? hours : 12; // Set 12 for midnight
 
-const time = hours + ":" + (a.getMinutes() < 10 ? '0' : '') + a.getMinutes() + ":" + (a.getSeconds() < 10 ? '0' : '') + a.getSeconds() + ' ' + ampm;
+    const time = hours + ":" + (a.getMinutes() < 10 ? '0' : '') + a.getMinutes() + ":" + (a.getSeconds() < 10 ? '0' : '') + a.getSeconds() + ' ' + ampm;
 
-document.getElementById("time").innerHTML = time + "<br>on " + date;
-document.getElementById("year").innerHTML = a.getFullYear();
+    document.getElementById("time").innerHTML = time + "<br> <br> <br><br>on " + date;
+    document.getElementById("year").innerHTML = a.getFullYear();
 }, 1000);
